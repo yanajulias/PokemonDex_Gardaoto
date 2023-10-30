9c0fe8807605d7ebff7cae1c1ebead6b6b3bdeb8
Yana <44026935+yanajulias@users.noreply.github.com>
2023-10-31 02:55:27 +0700

    Create pokemon list screen

---
+++ app/src/main/java/gardaoto/projects/pokemondex/features/pokemonlist/PokemonListScreen.kt
@@ -0,0 +1,247 @@
+package gardaoto.projects.pokemondex.features.pokemonlist
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import gardaoto.projects.pokemondex.R
+import gardaoto.projects.pokemondex.domain.Pokemon
+
+@Composable
+fun PokemonListScreen(
+    navController: NavController
+) {
+    Surface(
+        color = MaterialTheme.colorScheme.background,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column {
+            Spacer(modifier = Modifier.height(20.dp))
+            Image(
+                painterResource(id = R.drawable.ic_pokemon_logo),
+                contentDescription = "Pokemon Logo",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            PokemonList(navController = navController)
+        }
+    }
+}
+
+@Composable
+fun PokemonList(
+    navController: NavController,
+    viewModel: PokemonListViewModel = hiltViewModel()
+) {
+    val pokemonList by remember { viewModel.pokemonList }
+    val endReached by remember { viewModel.endReached }
+    val loadError by remember { viewModel.loadError }
+    val isLoading by remember { viewModel.isLoading }
+
+    LazyColumn(contentPadding = PaddingValues(16.dp)) {
+        val itemCount = if (pokemonList.size % 2 == 0) {
+            pokemonList.size / 2
+        } else {
+            pokemonList.size / 2 + 1
+        }
+
+        items(itemCount) {
+            if (it >= itemCount - 1 && !endReached && !isLoading) {
+                viewModel.loadPokemonList()
+            }
+            PokemonRow(
+                rowIndex = it,
+                pokemonIndex = pokemonList,
+                navController = navController
+            )
+        }
+    }
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        if (isLoading) {
+            CircularProgressIndicator(color = MaterialTheme.colorScheme.primary)
+        }
+        if (loadError.isNotEmpty()) {
+            RetrySection(error = loadError) {
+                viewModel.loadPokemonList()
+            }
+        }
+    }
+}
+
+
+@Composable
+fun PokemonItem(
+    pokemon: Pokemon,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    viewModel: PokemonListViewModel = hiltViewModel()
+) {
+    val defaultDominantColor = MaterialTheme.colorScheme.surface
+    var dominantColor by remember {
+        mutableStateOf(defaultDominantColor)
+    }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .shadow(3.dp, RoundedCornerShape(10.dp))
+            .clip(RoundedCornerShape(10.dp))
+            .aspectRatio(1f)
+            .background(
+                Brush.verticalGradient(
+                    listOf(
+                        dominantColor,
+                        defaultDominantColor
+                    )
+                )
+            )
+            .clickable {
+                navController.navigate(
+                    "pokemon_detail_screen/${dominantColor.toArgb()}/${pokemon.pokemonName}"
+                )
+            }
+    ) {
+        Column {
+            val painter = rememberAsyncImagePainter(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(pokemon.imageUrl)
+                    .crossfade(true)
+
+                    .build(),
+            )
+
+            val state = painter.state
+
+            Image(
+                painter = painter,
+                contentDescription = pokemon.pokemonName,
+                modifier = Modifier
+                    .size(120.dp)
+                    .align(Alignment.CenterHorizontally)
+                    .fillMaxWidth(),
+            )
+            if (state is AsyncImagePainter.State.Success) {
+                viewModel.dominantColor(state.result.drawable) { color ->
+                    dominantColor = color
+                }
+            } else if (state is AsyncImagePainter.State.Loading) {
+                CircularProgressIndicator(
+                    color = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.scale(0.5f)
+                        .align(Alignment.CenterHorizontally)
+                )
+            }
+
+            Text(
+                text = pokemon.pokemonName,
+                fontSize = 16.sp,
+                textAlign = TextAlign.Center,
+                modifier = modifier
+                    .fillMaxSize()
+                    .padding(bottom = 8.dp),
+                maxLines = 2
+            )
+        }
+    }
+}
+
+
+@Composable
+fun PokemonRow(
+    rowIndex: Int,
+    pokemonIndex: List<Pokemon>,
+    navController: NavController,
+    modifier: Modifier = Modifier
+) {
+    Column {
+        Row {
+            PokemonItem(
+                pokemon = pokemonIndex[rowIndex * 2],
+                navController = navController,
+                modifier = modifier.weight(1f)
+            )
+            Spacer(modifier = modifier.width(16.dp))
+            if (pokemonIndex.size >= rowIndex * 2 + 2) {
+                PokemonItem(
+                    pokemon = pokemonIndex[rowIndex * 2 + 1],
+                    navController = navController,
+                    modifier = modifier.weight(1f)
+                )
+            } else {
+                Spacer(modifier = modifier.width(16.dp))
+            }
+        }
+        Spacer(modifier = modifier.height(16.dp))
+    }
+}
+
+@Composable
+fun RetrySection(
+    error: String,
+    onRetry: () -> Unit
+) {
+    Column {
+        Text(error, color = Color.Red, fontSize = 18.sp)
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(
+            onClick = { onRetry() },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            Text(text = "Retry")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PokemonListScreenPreview() {
+    PokemonListScreen(navController = rememberNavController())
+}
