7c6a5a05be864d547f8a83b912023b0492c54aac
Yana <44026935+yanajulias@users.noreply.github.com>
2023-10-31 02:54:41 +0700

    Create navigation in MainActivity

--- app/src/main/java/gardaoto/projects/pokemondex/MainActivity.kt
+++ app/src/main/java/gardaoto/projects/pokemondex/MainActivity.kt
@@ -3,44 +3,63 @@ package gardaoto.projects.pokemondex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import dagger.hilt.android.AndroidEntryPoint
+import gardaoto.projects.pokemondex.features.pokemondetail.PokemonDetailScreen
+import gardaoto.projects.pokemondex.features.pokemonlist.PokemonListScreen
 import gardaoto.projects.pokemondex.ui.theme.PokemonDexTheme
+import java.util.Locale
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PokemonDexTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "pokemon_list_screen")
+                {
+                    composable("pokemon_list_screen") {
+                        PokemonListScreen(navController = navController)
+                    }
+
+                    composable(
+                        "pokemon_detail_screen/{dominantColor}/{pokemonName}",
+                        arguments = listOf(
+                            navArgument("dominantColor") {
+                                type = NavType.IntType
+                            },
+                            navArgument("pokemonName") {
+                                type = NavType.StringType
+                            }
+                        )
+                    ) {
+                        val dominantColor = remember {
+                            val color = it.arguments?.getInt("dominantColor")
+                            color?.let { Color(it) } ?: Color.White
+                        }
+
+                        val pokemonName = remember {
+                            it.arguments?.getString("pokemonName")
+                        }
+
+                        if (pokemonName != null) {
+                            PokemonDetailScreen(
+                                dominantColor = dominantColor,
+                                pokemonName = pokemonName.lowercase(Locale(localClassName)),
+                                navController = navController
+                            )
+                        }
+
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PokemonDexTheme {
-        Greeting("Android")
     }
 }
