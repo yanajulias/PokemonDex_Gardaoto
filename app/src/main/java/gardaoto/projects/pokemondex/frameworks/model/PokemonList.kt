5d4d2db5d6961ebc6fda631bfa6af95d130ee619
Yana <44026935+yanajulias@users.noreply.github.com>
2023-10-31 02:51:25 +0700

    Create pokemon list and detail response

---
+++ app/src/main/java/gardaoto/projects/pokemondex/frameworks/model/PokemonList.kt
@@ -0,0 +1,21 @@
+package gardaoto.projects.pokemondex.frameworks.model
+
+import com.google.gson.annotations.SerializedName
+
+data class PokemonList(
+    @SerializedName("count")
+    val count: Int?,
+    @SerializedName("next")
+    val next: String?,
+    @SerializedName("previous")
+    val previous: Any?,
+    @SerializedName("results")
+    val results: List<Pokemon?>
+) {
+    data class Pokemon(
+        @SerializedName("name")
+        val name: String?,
+        @SerializedName("url")
+        val url: String?
+    )
+}
